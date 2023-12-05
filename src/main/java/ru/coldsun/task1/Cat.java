4bac335b430c0d51a1824717108a1e3b984e2dc6
aleksandr_sa <coldsun93@mail.ru>
2023-12-05 23:13:50 +0300

    добавлено решение по первому заданию

---
+++ src/main/java/ru/coldsun/task1/Cat.java
@@ -0,0 +1,23 @@
+package ru.coldsun.task1;
+
+public class Cat extends Animal {
+    public Boolean presenceWool;
+
+    public  Cat(String name, int age, Boolean presenceWool) {
+        super(name,age);
+        this.presenceWool = presenceWool;
+    }
+
+    public void makeSounds() {
+
+        System.out.println("Мяу-Мяу");
+
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "presenceWool=" + presenceWool +
+                '}';
+    }
+}
---
+++ src/main/java/ru/coldsun/task1/Dog.java
@@ -0,0 +1,19 @@
+package ru.coldsun.task1;
+
+public class Dog extends Animal{
+    public Boolean trained;
+
+    public  Dog(String name, int age, Boolean trained) {
+        super(name,age);
+        this.trained = trained;
+    }
+
+    public  Dog(String name, int age) {
+        super(name,age);
+    }
+
+
+    public void makeSounds() {
+        System.out.println("Гав-Гав");
+    }
+}
